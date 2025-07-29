d545c7e352bfdd83922e7b654168bb75c4e8e7e7
Frank Tuan <franktuan@microsoft.com>
2025-07-29 12:38:45 -0700

    Minor change

--- src/routes/api/time.py
+++ src/routes/api/time.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from fastapi import APIRouter
 
@@ -8,6 +8,6 @@ def time_routes():
 
     @router.get("/api/current_time")
     async def get_current_time():
-        return {"current_time": datetime.now().isoformat()}
+        return {"current_time": datetime.now(tz=timezone.utc).isoformat(timespec="seconds")}
 
     return router
